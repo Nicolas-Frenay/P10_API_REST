8989c27498cd8ae2480671f648c629a19afe1590
MilhouseVanProuten <milhousevanprouten@gmail.com>
2021-12-14 12:55:21 +0100

    move some logic from contributors view to serializer

--- apps/contributors/serializers.py
+++ apps/contributors/serializers.py
@@ -1,5 +1,7 @@
-from rest_framework.serializers import ModelSerializer, SerializerMethodField
+from rest_framework.serializers import ModelSerializer, \
+    SerializerMethodField, ValidationError
 from apps.contributors.models import Contributor
+from apps.projects.models import Project
 from django.contrib.auth.models import User
 
 
@@ -12,8 +14,29 @@ class ContributorSerializer(ModelSerializer):
 
     def get_users(self, instance):
         queryset = instance.user_id
-        serializer = UserSerializer(queryset, many=True)
+        serializer = UserSerializer(queryset)
         return serializer.data
+
+    def create(self, validated_data):
+        new_username = self.context['request'].data['new_user']
+        role = self.context['request'].data['role']
+        project_id = self.context['project']
+        project = Project.objects.get(id=project_id)
+
+        try:
+            new_user = User.objects.get(username=new_username)
+            try:
+                if Contributor.objects.get(project_id=project,
+                                           user_id=new_user):
+                    raise ValidationError(
+                        'User already working on this project')
+            except Contributor.DoesNotExist:
+                contrib = Contributor.objects.create(project_id=project,
+                                                     role=role,
+                                                     user_id=new_user)
+                return contrib
+        except User.DoesNotExist:
+            raise ValidationError('Invalid user name')
 
 
 class UserSerializer(ModelSerializer):
--- apps/contributors/views.py
+++ apps/contributors/views.py
@@ -2,33 +2,23 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from apps.contributors.serializers import ContributorSerializer
 from apps.contributors.models import Contributor
-from apps.projects.models import Project
-from django.contrib.auth.models import User
 from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
 
 
-
 class UserViewset(ModelViewSet):
     serializer_class = ContributorSerializer
-
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         return Contributor.objects.filter(project_id=self.kwargs['project_pk'])
 
-    def create(self, request, *args, **kwargs):
-        new_username = request.data['new_user']
-        project = Project.objects.get(id=self.kwargs['project_pk'])
-        try:
-            new_user = User.objects.get(username=new_username)
-            contrib = Contributor.objects.create(project_id=project,
-                                                 role='CONTRIBUTOR')
-            contrib.user_id.set([new_user.id, ])
-            contrib.save()
-            return Response(data='New user added', status=200)
-        except User.DoesNotExist:
-            raise NotFound('Invalid user name')
+    def get_serializer_context(self):
+        project = self.kwargs['project_pk']
+        context = super(UserViewset, self).get_serializer_context()
+        context.update({'project': project})
+        context.update({'request': self.request})
+        return context
 
     def destroy(self, request, *args, **kwargs):
         try:
