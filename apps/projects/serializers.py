38ffce362a4b711ead9f782a28134e005893569b
MilhouseVanProuten <milhousevanprouten@gmail.com>
2021-12-15 19:18:30 +0100

    add details issue and contributor in project detail view

--- apps/projects/serializers.py
+++ apps/projects/serializers.py
@@ -1,12 +1,15 @@
 from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from apps.projects.models import Project
 from apps.contributors.models import Contributor
+from apps.contributors.serializers import ContributorSerializer
 from apps.issues.models import Issue
+from apps.issues.serializers import IssueSerializer
 
 
 class ProjectSerializer(ModelSerializer):
     contributors_count = SerializerMethodField(read_only=True)
     issues_count = SerializerMethodField(read_only=True)
+
     class Meta:
         model = Project
         fields = ('id', 'title', 'description', 'proj_type',
@@ -30,3 +33,25 @@ class ProjectSerializer(ModelSerializer):
         return(project)
 
     # TODO: voire pour partial update avec PUT
+
+
+class ProjectDetailSerializer(ModelSerializer):
+    contributors = SerializerMethodField(read_only=True)
+    issues = SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Project
+        fields = ('id', 'title', 'description', 'proj_type',
+                  'issues', 'contributors')
+
+    def get_contributors(self, instance):
+        project = instance.id
+        queryset = Contributor.objects.filter(project_id=project)
+        serializer = ContributorSerializer(queryset, many=True)
+        return serializer.data
+
+    def get_issues(self, instance):
+        project = instance.id
+        queryset = Issue.objects.filter(project_id=project)
+        serializer = IssueSerializer(queryset, many=True)
+        return serializer.data
--- apps/projects/views.py
+++ apps/projects/views.py
@@ -1,15 +1,23 @@
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
-from apps.projects.serializers import ProjectSerializer
+from apps.projects.serializers import ProjectSerializer, \
+    ProjectDetailSerializer
 from apps.projects.models import Project
+
 
 class ProjectViewset(ModelViewSet):
     serializer_class = ProjectSerializer
+    detail_serializer_class = ProjectDetailSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         return Project.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return self.detail_serializer_class
+        return super().get_serializer_class()
 
     def destroy(self, request, *args, **kwargs):
         super().destroy(request, *args, **kwargs)
