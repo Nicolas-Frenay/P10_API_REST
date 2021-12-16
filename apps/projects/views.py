9f80345f5d61a5a22d02f62c595cbf2e47521877
MilhouseVanProuten <milhousevanprouten@gmail.com>
2021-12-16 20:47:49 +0100

    correction : user now only see project he's a contributor to.

--- apps/projects/views.py
+++ apps/projects/views.py
@@ -1,9 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
+from SoftDesk.permissions import IsProjectAuthor, IsProjectContributor
+from apps.projects.models import Project
+from apps.contributors.models import Contributor
 from apps.projects.serializers import ProjectSerializer, \
     ProjectDetailSerializer
-from apps.projects.models import Project
-from SoftDesk.permissions import IsProjectAuthor, IsProjectContributor
 
 
 class ProjectViewset(ModelViewSet):
@@ -16,7 +17,11 @@ class ProjectViewset(ModelViewSet):
     SAFE_METHODS = ['list']
 
     def get_queryset(self):
-        return Project.objects.all()
+        user = self.request.user
+        queryset = Project.objects.filter(
+            id__in=Contributor.objects.filter(user_id=user.id).values_list(
+                'project_id'))
+        return queryset
 
     def get_serializer_class(self):
         if self.action == 'retrieve':
